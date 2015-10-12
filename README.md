4fcfc7fa96335a7c6591402d3237f510c795e7e0
kik0220 <kik0220@gmail.com>
2015-10-13 01:55:38 +0900

    use webapp2

--- README.md
+++
@@ -1,3 +0,0 @@
-finalurl
-========
-[finalurl](http://finalurl.appspot.com)
--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-from handlers import *
+import os
+import logging
+import webapp2
+from google.appengine.api import memcache, urlfetch
 
-def main():
-    application = webapp.WSGIApplication(
-        [
-            ('/api', ApiHandler),
-            ],
-        debug=True)
-    run_wsgi_app(application)
+class ApiHandler(webapp2.RequestHandler):
+    def get(self):
+        url = self.request.get('url')
+        if not url:
+            self.error(400)
+            self.response.out.write('url is required.')
+            return
 
-if __name__ == "__main__":
-        main()
+        final_url = url
+        try:
+            # has cache?
+            url_from_cache = memcache.get(url)
+            if url_from_cache is not None:
+                logging.info("cache hit: " + url)
+                final_url = url_from_cache
 
+            else:
+                # fetch
+                logging.info("fetch: " + url)
+                response = urlfetch.fetch(url = url, method = urlfetch.HEAD, follow_redirects = False)
+                final_url = response.headers['location'] if 'location' in response.headers else url
+                memcache.add(url, final_url, time = 86400)
+
+        except urlfetch.Error, error: # urlfetch.Error ???
+            logging.warn(error.args)
+
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(final_url)
+
+app = webapp2.WSGIApplication([
+    ('/api', ApiHandler),
+], debug=True)
